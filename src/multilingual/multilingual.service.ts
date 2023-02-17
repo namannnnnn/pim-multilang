1eac8506bb5f6bfc0a507b37389893c291875b91
namannnnnn <namanjp02@gmail.com>
2023-02-17 14:13:03 +0530

    34

--- src/multilingual/multilingual.service.ts
+++ src/multilingual/multilingual.service.ts
@@ -368,26 +368,27 @@ export class Translator {
             if (request.lang_code == 'en') {
                 let language = request.lang_code;
                 delete request.lang_code;
-                // let rule = await new RuleModel(request);
-                // data = await rule.save();
+                let rule = await new RuleModel(request);
+                data = await rule.save();
                 data._id = (data._id).toString();
                 for (let i = 0; i < config[0].selected_languages.length; i++) {
                     if (config[0].selected_languages[i] == 'en') {
                     }
                     else {
-                        (request.translations)[config[0].selected_languages[i]] = ((request.translations).en)
+                        (og_translation.translations)[config[0].selected_languages[i]] = ((request.translations).en)
                         for (let j = 0; j < toTranslate[0].translatable_fields.length; j++) {
                             if (toTranslate[0].translatable_fields[j] == 'conditions_2' || toTranslate[0].translatable_fields[j] == 'conditions_3' || toTranslate[0].translatable_fields[j] == 'setValue') {
                             }
                             else {
-                                (request.translations[config[0].selected_languages[i]])[toTranslate[0].translatable_fields[j]] = await this.translation(googleTranslator, ((request.translations).en)[toTranslate[0].translatable_fields[j]], config[0].selected_languages[i]);
+                                (og_translation.translations[config[0].selected_languages[i]])[toTranslate[0].translatable_fields[j]] = await this.translation(googleTranslator, ((request.translations).en)[toTranslate[0].translatable_fields[j]], config[0].selected_languages[i]);
                             }
                             
                         }
                     }
                 }
-                let rule = await new RuleModel(request);
-                data = await rule.save();
+                data = await RuleModel.updateOne({ _id : data._id }, { ...og_translation })
+                console.log(data)
+                // data = await rule.save();
             }
             else {
                 let language = request.lang_code;
